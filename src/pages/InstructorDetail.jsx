154baafdf42647d65a21d32a5fa28b21b74fbcca
Burakkturk <burak1845turk@icloud.com>
2024-05-10 00:30:09 +0300

    v1

---
+++ src/pages/InstructorDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InstructorDetail = () => {
+  return (
+    <div className='p-3'>InstructorDetail</div>
+  )
+}
+
+export default InstructorDetail
--- src/pages/Instructors.jsx
+++ src/pages/Instructors.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Instructors = () => {
   return (
     <div className="p-3">
       <h1>Instructors</h1>
+      <Link to="1">Instructor-1</Link><br></br>
+      <Link to="2">Instructor-2</Link><br></br>
+      <Link to="3">Instructor-3</Link>
     </div>
+    
   );
 };
 
